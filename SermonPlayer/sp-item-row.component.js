88e2f85b750629a6fc7e5355d795363d7203e065
Robert Murray <Robert.Murray@kronos.com>
2017-03-07 19:39:59 -0500

    Implemented ng-repeat on list

--- SermonPlayer/sp-item-row.component.js
+++ SermonPlayer/sp-item-row.component.js
@@ -12,34 +12,14 @@
 
   ItemRowController.$inject = ['SermonService'];
   function ItemRowController(SermonService) {
-
       //------------------------------------------------------------------------------------//
-      // Define irc as this because 'this' used in the RESPONSE means something else and I  //
-      // want the 'this' handle to the ItemRowController.                                   //
+      // SermonService is used to share sermonInfo between controllers.                     //
       //------------------------------------------------------------------------------------//
       var irc = this;
+      irc.sermonInfo = SermonService.getSermonListInfo();
 
-      var sermonInfo = SermonService.getSermonInfo();
-
-      irc.title=sermonInfo.title;
-      irc.speaker=sermonInfo.speaker;
-      irc.date=sermonInfo.date;
-      irc.filename=sermonInfo.filename;
-
-      irc.getSermonTitle = function() {
-          return irc.title;
-      }
-
-      irc.getSermonSpeaker = function() {
-          return irc.speaker;
-      }
-
-      irc.getSermonDate = function() {
-          return irc.date;
-      }
-
-      irc.getSermonFilename = function() {
-          return irc.filename;
+      irc.getSermonListInfo = function() {
+        return irc.sermonInfo;
       }
 
     }
