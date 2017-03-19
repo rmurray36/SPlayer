89246fea9f5fb00b459ac545de3ee6096c16def3
Robert Murray <Robert.Murray@kronos.com>
2017-03-19 13:33:13 -0400

    Change URL for server (temporary)

--- SermonPlayer/sermon.service.js
+++ SermonPlayer/sermon.service.js
@@ -13,7 +13,7 @@
 
         service.retrieveSermonListFromServer = function ()
         {
-            var url = "http://192.168.1.105:8080/SERMONPLAYER-RS/rest/getLatestSermon";
+            var url = "http://localhost:3200/audioFiles/allSermons";
 //          var url = "http://71.234.246.37:8080/SERMONPLAYER-RS/rest/getLatestSermon";
 //          var promise =  $http.get("http://192.168.1.105:8080/SERMONPLAYER-RS/rest/getLatestSermon").then(sermonInfoResponse).catch(sermonInfoError);
             var promise =  $http.get(url);
